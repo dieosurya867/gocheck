1ab677797f2992f00211c23a8f4dd55b64d2bbb4
dieosurya <dieosurya@gmail.com>
2025-08-09 19:02:36 +0700

    memindahkan state ke komponen paling atas untuk alternatif global state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 const listItems = [
   { id: 1, title: "Eat", done: false },
   { id: 2, title: "Sleep", done: true },
 ];
 
 function App() {
+  const [listItems, setListItems] = useState([]);
+
+  function handleAddItem(item) {
+    setListItems((listItems) => [...listItems, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <CheckList />
+      <Form onAddItem={handleAddItem} />
+      <CheckList items={listItems} />
       <Stats />
     </div>
   );
@@ -18,25 +26,41 @@ function Logo() {
   return <span className="logo">📝 GoCheck ✅</span>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
+  const [title, setTitle] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+    if (!title) return;
+
+    const newItem = {
+      id: Date.now(),
+      title,
+      done: false,
+    };
+
+    onAddItem(newItem);
+    setTitle("");
   }
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Ada yang mau kamu catat? 🤔</h3>
-      <input type="text" name="title" id="" />
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
 
-function CheckList() {
+function CheckList({ items }) {
   return (
     <div className="list">
       <ul>
-        {listItems.map((item) => (
+        {items.map((item) => (
           <Item key={item.id} item={item} />
         ))}
       </ul>
